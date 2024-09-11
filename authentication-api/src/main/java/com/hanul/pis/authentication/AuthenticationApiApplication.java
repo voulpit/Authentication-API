2c044df0a3d5458701e65932412e9ad090ce0c34
voulpit <kalimatix@yahoo.com>
2024-09-11 16:28:41 +0300

    Deployable to war

--- authentication-api/src/main/java/com/hanul/pis/authentication/AuthenticationApiApplication.java
+++ authentication-api/src/main/java/com/hanul/pis/authentication/AuthenticationApiApplication.java
@@ -3,12 +3,19 @@ package com.hanul.pis.authentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class AuthenticationApiApplication {
+public class AuthenticationApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AuthenticationApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationApiApplication.class, args);
--- authentication-api/src/main/java/com/hanul/pis/authentication/infra/repo/UserRepository.java
+++ authentication-api/src/main/java/com/hanul/pis/authentication/infra/repo/UserRepository.java
@@ -1,12 +1,11 @@
 package com.hanul.pis.authentication.infra.repo;
 
 import com.hanul.pis.authentication.infra.entity.UserEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long>, PagingAndSortingRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByEmail(String email);
     UserEntity findByUserId(String userId);
 }
